842a3c3f9c00ce87f984e8c09de82155ce432d3e
elias <14819853+eliasthecactus@users.noreply.github.com>
2024-01-28 23:03:34 +0100

    snapshot

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from MySQLdb import IntegrityError
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, create_refresh_token
 from datetime import datetime, timedelta
 import os
 import secrets
@@ -10,9 +10,15 @@ import bcrypt
 from uuid import uuid4
 import binascii
 import re
+import imghdr
+from PIL import Image
+from kmlparser import *
+
 
 app = Flask(__name__)
 CORS(app)
+
+raw_profile_picture_path = 'profile_pictures'
 
 # Configure database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://user:password@127.0.0.1:3306/db'
@@ -40,6 +46,7 @@ class User(db.Model):
     reset_token_time = db.Column(db.DateTime)
     deleted = db.Column(db.Boolean, default=False)
     deleted_at = db.Column(db.DateTime)
+    pending_email = db.Column(db.String(120))
     info = db.Column(db.String(255))
 
 class Flight(db.Model):
@@ -58,17 +65,9 @@ class Flight(db.Model):
 
 
 
-@app.errorhandler(401)
-def token_expired(error):
-    return jsonify({"message": "Token has expired"}), 401
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return jsonify({"message": "404 Not Found"}), 404
-
-@app.errorhandler(500)
-def internal_server_error(error):
-    return jsonify({"message": "Internal Server Error"}), 500
+@jwt.expired_token_loader
+def my_expired_token_callback(jwt_header, jwt_payload):
+    return jsonify(code="99", message="Token expired"), 200
 
 # @app.errorhandler(Exception)
 # def generic_error(error):
@@ -83,7 +82,15 @@ def ping():
 @jwt_required()
 def auth_ping():
     current_user = get_jwt_identity()
-    return jsonify({"message": "pong, you are nr "+str(current_user)}), 200
+    return jsonify(code='0', message='pong'), 200
+
+@app.route('/api/user/refresh', methods=['GET'])
+@jwt_required()
+def refresh_token():
+    current_user = get_jwt_identity()
+    new_access_token = create_access_token(identity=current_user)
+    return jsonify(code="0", access_token=new_access_token), 200
+
 
 @app.route('/api/user/availability', methods=['GET'])
 def check_availability():
@@ -110,17 +117,16 @@ def register():
     password = data.get('password')
 
     if not email or not username or not password or not first_name or not last_name:
-        return jsonify(code='30', message='First name, Last name, Email, username, and password are required'), 400
+        return jsonify(code='30', message='First name, Last name, Email, username, and password are required'), 200
     
-    # Validation: Check if the email and username are unique
     if User.query.filter_by(email=email).first():
-        return jsonify(code='20', message='Email already in use'), 400
+        return jsonify(code='20', message='Email already in use'), 200
     
     if User.query.filter_by(username=username).first():
-        return jsonify(code='10', message='Username already in use'), 400
+        return jsonify(code='10', message='Username already in use'), 200
     
     if len(password) < 8 or not any(char.islower() for char in password) or not any(char.isupper() for char in password) or not re.compile(r'[!@#$%^&*(),.?":{}|<>]').search(password):
-        return jsonify(code='40', message='Password to weak'), 400
+        return jsonify(code='40', message='Password to weak'), 200
     
     encoded_password = password.encode('utf-8')
 
@@ -140,11 +146,10 @@ def register():
     )
 
     try:
-        # Add the new user to the database
         db.session.add(new_user)
         db.session.commit()
-        # send activation mail
-        return jsonify(message='User registered successfully'), 201
+        # tbd send activation mail
+        return jsonify(code="0", message='User registered successfully'), 200
     except IntegrityError as e:
         db.session.rollback()
         return jsonify(message=f'Error registering user: {str(e)}'), 500
@@ -165,8 +170,11 @@ def login():
         if bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')):
             if (user.activated == True):
                 if (user.enabled == True):
-                    access_token = create_access_token(identity=user.id)
-                    return jsonify(code='0', access_token=access_token)
+                    if (user.deleted == False):
+                        access_token = create_access_token(identity=user.id)
+                        return jsonify(code='0', access_token=access_token)
+                    else:
+                        return jsonify(code='40', message='Account deleted'), 200
                 else:
                     return jsonify(code='10', message='Account disabled'), 200
             else:
@@ -177,61 +185,132 @@ def login():
     else:
         return jsonify(code='30', message='Invalid credentials'), 200
 
+
 @app.route('/api/user/resetpassword', methods=['POST'])
 def password_forgot():
+    
     data = request.get_json()
+    
     email = data.get('email')
+    token = data.get('token')
+    password = data.get('password')
 
-    #tbd
+    if email:
+        # tbd send email with token
+        return jsonify(code="0", message='Password reset link sent successfully'), 200
+    
+    if token:
+        if password:
+            # tbd validate token and reset reset password
+
+            return jsonify(code="10", message='Password reset link sent successfully'), 200
+
+        else:
+            return jsonify(code="10", message='Password is missing'), 200
+    else:
+        return jsonify(code="20", message='Email/Token is missing'), 200
 
 
 
-    return jsonify(message='Password reset link sent successfully')
+
+# tbd resend activation email
+
+@app.route('/api/user/changepassword', methods=['POST'])
+@jwt_required()
+def password_change():
+    current_user = get_jwt_identity()
+    data = request.get_json()
+    currentPassword = data.get('currentPassword')
+    newPassword = data.get('newPassword')
+
+    try:
+        user = User.query.get(current_user)
+        if user:
+            if currentPassword:
+                if newPassword:
+                    if len(newPassword) < 8 or not any(char.islower() for char in newPassword) or not any(char.isupper() for char in newPassword) or not re.compile(r'[!@#$%^&*(),.?":{}|<>]').search(newPassword):
+                        return jsonify(code='40', message='Password to weak'), 200
+                    hashed_password = user.password
+                    if bcrypt.checkpw(currentPassword.encode('utf-8'), hashed_password.encode('utf-8')):
+                        new_hashed_password = bcrypt.hashpw(newPassword.encode('utf-8'), user.salt.encode('utf-8'))
+
+                        user.password = new_hashed_password
+                        db.session.commit()
+                        return jsonify(code='0', message='Password updated successfully'), 200
+                    else:
+                        return jsonify(code='50', message='Invalid credentials'), 200
+
+                else:
+                    return jsonify(code='40', message='Please provide the new password'), 200
+            else:
+                return jsonify(code='30', message='Please provide the current password'), 200
+        else:
+            return jsonify(code='20', message='There was an error while updating the account'), 200
+    except IntegrityError as e:
+        db.session.rollback()
+        return jsonify(message=f'Error updating account: {str(e)}'), 500
+
 
 @app.route('/api/user/activate', methods=['GET'])
 def activate_account():
     activation_token = request.args.get('token')
 
-    #tbd
+    if not activation_token:
+        return jsonify(code='10', message='Activation token is missing'), 400
 
+    user = User.query.filter_by(activation_token=activation_token).first()
 
-    return jsonify(message='Activated successfully')
+    if user:
+        user.activated = True
+        user.activation_token = None
+        if user.pending_email != None:
+            user.email = user.pending_email
+            user.pending_email = None
+        db.session.commit()
+
+        return jsonify(code='0', message='Account activated successfully'), 200
+    else:
+        return jsonify(code='20', message='Invalid activation token'), 400
 
 @app.route('/api/user', methods=['PUT'])
 @jwt_required()
 def update_account():
     current_user = get_jwt_identity()
-    user_id = current_user.get('id')
-
     data = request.get_json()
     new_first_name = data.get('first_name')
     new_last_name = data.get('last_name')
     new_username = data.get('username')
+    new_email = data.get('email')
 
 
     try:
-        user = User.query.get(user_id)
+        user = User.query.get(current_user)
         if user:
-            # Update the first name if provided
             if new_first_name:
                 user.first_name = new_first_name
 
-            # Update the last name if provided
             if new_last_name:
                 user.last_name = new_last_name
 
-            # Update the username if provided and unique
             if new_username:
-                if User.query.filter(User.id != user_id, User.username == new_username).first():
-                    return jsonify(message='Username already in use'), 400
-                user.username = new_username
+                if User.query.filter(User.id != current_user, User.username == new_username).first():
+                    return jsonify(code='10',message='Username already in use'), 200
+                if user.username != new_username:
+                    user.username = new_username
 
-            # Your implementation to update other information if needed
+            if new_email:
+                if User.query.filter(User.id != current_user, User.username == new_username).first():
+                    return jsonify(code='20',message='There is already an account with this Email-Address'), 200
+                if user.email != new_email:
+                    user.pending_email = new_email
+                    user.activation_token = binascii.hexlify(os.urandom(20)).decode()
 
             db.session.commit()
-            return jsonify(message='Account updated successfully')
+            #tbd send mail with token
+
+            return jsonify(code='0', message='Account updated successfully'), 200
         else:
-            return jsonify(message='User not found'), 404
+            return jsonify(code='30', message='There was an error while updating the account'), 200
     except IntegrityError as e:
         db.session.rollback()
         return jsonify(message=f'Error updating account: {str(e)}'), 500
@@ -240,10 +319,168 @@ def update_account():
 @jwt_required()
 def delete_account():
     current_user = get_jwt_identity()
+    # tbd token should be invalid after delete
+    try:
+        # print(current_user)
+        user = User.query.get(current_user)
 
-    #tbd
+        if user:
+            # Mark the user as deleted
+            user.deleted = True
+            user.deleted_at = datetime.utcnow()
 
-    return jsonify(code='0', message='Account deleted successfully')
+            #tbd delete profile picture...
+            db.session.commit()
+
+            return jsonify(code='0', message='Account deleted successfully'), 200
+        else:
+            return jsonify(code='10', message='User not found'), 404
+    except Exception as e:
+        db.session.rollback()
+        return jsonify(code='20', message=f'Error deleting account: {str(e)}'), 500
+    
+@app.route('/api/user', methods=['GET'])
+@jwt_required()
+def get_account():
+    current_user = get_jwt_identity()
+    # tbd token should be invalid after delete
+    try:
+        print(current_user)
+        user = User.query.get(current_user)
+
+        if user:
+            return jsonify(code='0', email=user.email, username=user.username, first_name=user.first_name, last_name=user.last_name, picture=user.profile_picture), 200
+        else:
+            return jsonify(code='10', message='User not found'), 404
+    except Exception as e:
+        db.session.rollback()
+        return jsonify(code='20', message=f'Error while getting account information: {str(e)}'), 500
+
+@app.route('/api/user/image', methods=['POST'])
+@jwt_required()
+def change_profile_picture():
+    current_user = get_jwt_identity()
+
+    # # take the image, save it to a temporary location (temp_images), crop it so it is a square, convert it to a png, resize it to 300x300, generate a random name (35 characters long), rename it to the randomname.png, save it to the image location (./profile_pictures/), add the random name to the database
+    try:
+        user = User.query.get(current_user)
+
+        if user:
+            if 'file' not in request.files:
+                return jsonify(code='10', message='No file part'), 200
+            
+            file = request.files['file']
+
+            if file.filename == '':
+                return jsonify(code='20', message='No selected file'), 200
+            
+
+            os.makedirs(raw_profile_picture_path, exist_ok=True)
+
+
+            if imghdr.what(file) not in ['jpeg', 'png']:
+                return jsonify(code='10', message='Not a valid file. Please use a png or jpg/jpeg'), 200
+            
+
+            image_type = str(imghdr.what(file))
+            filename = secrets.token_hex(16) + '.' + image_type.lower()
+            file_path = os.path.join(raw_profile_picture_path, filename)
+
+
+
+            img = Image.open(file)
+            min_dimension = min(img.size)
+
+
+            left = (img.width - min_dimension) // 2
+            top = (img.height - min_dimension) // 2
+            right = (img.width + min_dimension) // 2
+            bottom = (img.height + min_dimension) // 2
+            img_cropped = img.crop((left, top, right, bottom))
+
+
+            img_resized = img_cropped.resize((512, 512))
+
+
+            img_resized.save(file_path, quality=50)
+
+            old_profile_picture_filename = user.profile_picture
+            if old_profile_picture_filename:
+                old_profile_picture_path = os.path.join(raw_profile_picture_path, old_profile_picture_filename)
+                if os.path.exists(old_profile_picture_path):
+                    os.remove(old_profile_picture_path)
+
+            # file.save(file_path)
+
+            user.profile_picture = filename
+            db.session.commit()
+
+            return jsonify(code='0', imageid=filename, message='Profile picture updated successfully'), 200
+        else:
+            return jsonify(code='40', message='User not found'), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify(code='50', message=f'Error updating profile picture: {str(e)}'), 500
+    
+@app.route('/api/user/image', methods=['DELETE'])
+@jwt_required()
+def delete_profile_picture():
+    current_user = get_jwt_identity()
+
+    try:
+        user = User.query.get(current_user)
+        if user:
+            profile_picture_filename = user.profile_picture
+
+            if profile_picture_filename:
+
+                # Construct the full path to the profile picture file
+                profile_picture_path = os.path.join(raw_profile_picture_path, profile_picture_filename)
+
+                if os.path.exists(profile_picture_path):
+                    os.remove(profile_picture_path)
+                else:
+                    return jsonify(code='30', message='There was an error while deleting the profile picture. Please contact us or try later.'), 200
+
+
+                user.profile_picture = None
+                db.session.commit()
+
+                return jsonify(code='0', message='Profile picture removed successfully'), 200
+            else:
+                # User does not have a profile picture
+                return jsonify(code='10', message='User does not have a profile picture'), 200
+        else:
+            return jsonify(code='20', message='User not found'), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify(code='30', message=f'Error removing profile picture: {str(e)}'), 500
+
+@app.route('/api/user/image', methods=['GET'])
+# @jwt_required()
+def get_profile_picture():
+    # current_user = get_jwt_identity()
+
+    try:
+    #     user = User.query.get(current_user)
+
+    #     if user:
+        profile_picture_filename = request.args.get('filename')
+
+        if not profile_picture_filename:
+            return jsonify(code='10', message='Filename is missing in the request'), 200
+
+
+        profile_picture_path = os.path.join(raw_profile_picture_path, profile_picture_filename)
+        
+        if os.path.exists(profile_picture_path):
+            return send_from_directory(raw_profile_picture_path, profile_picture_filename)
+        else:
+            return jsonify(code='20', message='Requested profile picture not found'), 404
+            # else:
+            #     return jsonify(code='40', message='User not found'), 200
+    except Exception as e:
+        return jsonify(code='50', message=f'Error retrieving profile picture: {str(e)}'), 500
 
 
 
@@ -256,6 +493,8 @@ def upload_flight():
 
     # Your implementation to handle file upload, extract data, and store in the database
     # ...
+
+    # tbd multiple files (folder recursive)
 
     return jsonify(message='Flight uploaded successfully')
 
--- kmlparser.py
+++ kmlparser.py
@@ -10,49 +10,48 @@ meassures_per_second = 2
 
 
 def parse_kml(file_path):
+    try:
+        data = {}
 
-        try:
-            data = {}
+        tree = ET.parse(file_path)
+        root = tree.getroot()
 
-            tree = ET.parse(file_path)
-            root = tree.getroot()
-
-            for elem in root.iter():
-                coordinates = []
-                if 'name' in elem.tag:
-                    if (re.match(r'^Track .*', elem.text)):
-                        match = re.search(r"Track (.*) .*?, (.*?) (\d\d:\d\d:\d\d)", elem.text)
-                        flightUser = match.group(1)
-                        flightDate = dateparser.parse(match.group(2)).date()
-                        flightTime = dateparser.parse(match.group(3)).time()
+        for elem in root.iter():
+            coordinates = []
+            if 'name' in elem.tag:
+                if (re.match(r'^Track .*', elem.text)):
+                    match = re.search(r"Track (.*) .*?, (.*?) (\d\d:\d\d:\d\d)", elem.text)
+                    flightUser = match.group(1)
+                    flightDate = dateparser.parse(match.group(2)).date()
+                    flightTime = dateparser.parse(match.group(3)).time()
                         
-                        data['user'] = flightUser
-                        data['date'] = str(flightDate)
-                        data['time'] = str(flightTime)
+                    data['user'] = flightUser
+                    data['date'] = str(flightDate)
+                    data['time'] = str(flightTime)
 
-                if 'coordinates' in elem.tag:
-                    for line in elem.text.splitlines():
-                        templist = []
-                        if line != "":
-                            for coordinate in line.split(','):
-                                templist.append(float(coordinate))
-                            coordinates.append(templist)
+            if 'coordinates' in elem.tag:
+                for line in elem.text.splitlines():
+                    templist = []
+                    if line != "":
+                        for coordinate in line.split(','):
+                            templist.append(float(coordinate))
+                        coordinates.append(templist)
 
-            # data['coordinates'] = coordinates
+        # data['coordinates'] = coordinates
 
-            tzfinder = TimezoneFinder() 
-            tz = tzfinder.timezone_at(lng=coordinates[0][0], lat=coordinates[0][1])
-            data['timezone'] = tz
+        tzfinder = TimezoneFinder() 
+        tz = tzfinder.timezone_at(lng=coordinates[0][0], lat=coordinates[0][1])
+        data['timezone'] = tz
 
-            data['meassure_points'] = len(coordinates)
+        data['meassure_points'] = len(coordinates)
 
-            data['status'] = 200
-            data['message'] = "Everything fine"
-            return data
+        data['status'] = 200
+        data['message'] = "Everything fine"
+        return data
         
-        except Exception as e:
-            print(e)
-            return False
+    except Exception as e:
+        print(e)
+        return False
 
 
 
